dca74ca195460956c1c4a5fff6a739099bdbe90d
hhr <huhaoran2002@qq.com>
2023-07-05 09:17:47 +0800

    寄存器堆不需要复位

--- proj_miniRV/proj_single_cycle/proj_single_cycle.srcs/sources_1/new/ID.v
+++ proj_miniRV/proj_single_cycle/proj_single_cycle.srcs/sources_1/new/ID.v
@@ -46,44 +46,7 @@ module ID (
     assign RF_wR = IROM_inst[11:7];
     assign RF_we = CU_rf_we;
     // 同步写
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            for (iii = 0; iii < 32; iii = iii + 1) begin
-                RF_reg[iii] <= 32'b0;
-            end
-            // RF_reg[0]  <= 32'b0;
-            // RF_reg[1]  <= 32'b0;
-            // RF_reg[2]  <= 32'b0;
-            // RF_reg[3]  <= 32'b0;
-            // RF_reg[4]  <= 32'b0;
-            // RF_reg[5]  <= 32'b0;
-            // RF_reg[6]  <= 32'b0;
-            // RF_reg[7]  <= 32'b0;
-            // RF_reg[8]  <= 32'b0;
-            // RF_reg[9]  <= 32'b0;
-            // RF_reg[10] <= 32'b0;
-            // RF_reg[11] <= 32'b0;
-            // RF_reg[12] <= 32'b0;
-            // RF_reg[13] <= 32'b0;
-            // RF_reg[14] <= 32'b0;
-            // RF_reg[15] <= 32'b0;
-            // RF_reg[16] <= 32'b0;
-            // RF_reg[17] <= 32'b0;
-            // RF_reg[18] <= 32'b0;
-            // RF_reg[19] <= 32'b0;
-            // RF_reg[20] <= 32'b0;
-            // RF_reg[21] <= 32'b0;
-            // RF_reg[22] <= 32'b0;
-            // RF_reg[23] <= 32'b0;
-            // RF_reg[24] <= 32'b0;
-            // RF_reg[25] <= 32'b0;
-            // RF_reg[26] <= 32'b0;
-            // RF_reg[27] <= 32'b0;
-            // RF_reg[28] <= 32'b0;
-            // RF_reg[29] <= 32'b0;
-            // RF_reg[30] <= 32'b0;
-            // RF_reg[31] <= 32'b0;
-        end
+    always @(posedge clk) begin
         if (RF_we) begin
             case (CU_rf_wsel)
                 // WB_ALU
